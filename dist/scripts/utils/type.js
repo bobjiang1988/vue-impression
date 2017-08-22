4df588a0489ec45203871c0966fadadf42670354
Bob.J <bob_jiang@yeah.net>
2017-08-22 17:43:55 +0800

    reset vue-impression

--- dist/scripts/utils/type.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var isArray = exports.isArray = function isArray(array) {
-  return Object.prototype.toString.call(array) === '[object Array]';
-};

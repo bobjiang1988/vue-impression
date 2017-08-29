b14066efebd3f5e6317d80cb2b076ce2601bf333
Bob.J <bob_jiang@yeah.net>
2017-08-29 16:58:33 +0800

    cell 增加subheader; group 去掉padding; icon

--- src/scripts/components/Cell.vue
+++ src/scripts/components/Cell.vue
@@ -1,70 +1,94 @@
 <template>
-    <router-link
-        v-if="!disabled && to"
+    <router-link v-if="!disabled && to"
         :to="to"
         class="cell cell-link"
         :class="{
-            'cell-disabled': disabled,
-            'cell-no-gap': !hasGap,
-        }">
-        <div class="cell-header" v-if="$slots.header">
+                                            'cell-disabled': disabled,
+                                            'cell-no-gap': !hasGap,
+                                        }">
+        <div class="cell-header"
+            v-if="$slots.header">
             <slot name="header"></slot>
         </div>
-        <span class="cell-body">
+        <span class="cell-body cell-sub-header-panel"
+            v-if="$slots.subheader">
+            <div class="cell-sub-header">
+                <slot name="subheader"></slot>
+            </div>
+            <div class="cell-sub-body">
+                <slot></slot>
+            </div>
+        </span>
+        <span class="cell-body"
+            v-else>
             <slot></slot>
         </span>
-        <div class="cell-footer" v-if="$slots.footer">
+        <div class="cell-footer"
+            v-if="$slots.footer">
             <slot name="footer"></slot>
         </div>
-        <i v-if="hasArrow" class="icon icon-right cell-arrow" />
+        <i v-if="hasArrow"
+            class="icon icon-right cell-arrow" />
     </router-link>
 
-    <a
-        v-else
+    <a v-else
         :href="!disabled && href"
         @click="!disabled && $emit('click')"
         class="cell"
         :class="{
-            'cell-link': clickable,
-            'cell-disabled': disabled,
-            'cell-no-gap': !hasGap,
-        }" >
-        <div class="cell-header" v-if="$slots.header">
+                                            'cell-link': clickable,
+                                            'cell-disabled': disabled,
+                                            'cell-no-gap': !hasGap,
+                                        }">
+        <div class="cell-header"
+            v-if="$slots.header">
             <slot name="header"></slot>
         </div>
-        <span class="cell-body">
+        <span class="cell-body cell-sub-header-panel"
+            v-if="$slots.subheader">
+            <div class="cell-sub-header">
+                <slot name="subheader"></slot>
+            </div>
+            <div class="cell-sub-body">
+                <slot></slot>
+            </div>
+        </span>
+        <span class="cell-body"
+            v-else>
             <slot></slot>
         </span>
-        <div class="cell-footer" v-if="$slots.footer">
+        <div class="cell-footer"
+            v-if="$slots.footer">
             <slot name="footer"></slot>
         </div>
-        <i v-if="hasArrow" class="icon icon-right cell-arrow" ></i>
+        <i v-if="hasArrow"
+            class="icon icon-right cell-arrow"></i>
     </a>
 </template>
 
 <script>
-    export default {
-        name: 'cell',
-        props: {
-            to: [String, Object],
-            href: String,
-            disabled: Boolean,
-            hasGap: {
-                type: Boolean,
-                default: true,
-            },
-            hasArrow: {
-                type: Boolean,
-                default() {
-                    // 有href和to时默认为true
-                    return !!(this.href || this.to);
-                },
+export default {
+    name: 'cell',
+    props: {
+        to: [String, Object],
+        href: String,
+        disabled: Boolean,
+        hasGap: {
+            type: Boolean,
+            default: true,
+        },
+        hasArrow: {
+            type: Boolean,
+            default() {
+                // 有href和to时默认为true
+                return !!(this.href || this.to);
             },
         },
-        computed: {
-            clickable() {
-                return !this.disabled && (this.href || this._events.click);
-            },
+    },
+    computed: {
+        clickable() {
+            return !this.disabled && (this.href || this._events.click);
         },
-    };
+    },
+};
 </script>
